c7d1e528cb588eaed5b1b263a0bbd73d38f0eb16
HefnySco <mohammad.hefny@gmail.com>
2022-10-26 12:18:25 +0200

    version_2.2.1

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 1
+#define VERSION_MINOR 2
 #define REVISION 1
 #define STRINGIFY(x) #x
 #define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
