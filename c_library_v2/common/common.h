b54673f5ac2d35aeb21005fbf168b47f6eeeed33
HefnySco <mohammad.hefny@gmail.com>
2022-11-19 12:09:20 +0200

    fix: mavlink update

--- c_library_v2/common/common.h
+++ c_library_v2/common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH -2193124690818314258
+#define MAVLINK_COMMON_XML_HASH -2064213116845307066
 
 #ifdef __cplusplus
 extern "C" {
--- c_library_v2/storm32/storm32.h
+++ c_library_v2/storm32/storm32.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_STORM32.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_STORM32_XML_HASH 1196023962614496673
+#define MAVLINK_STORM32_XML_HASH 396733204324097562
 
 #ifdef __cplusplus
 extern "C" {
