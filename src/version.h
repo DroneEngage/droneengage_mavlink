6dfc4df9a4c214526c6ac5b31c4d1d595cc38b37
Mohammad Hefny <mohammad.hefny@gmail.com>
2022-12-17 12:31:45 +0200

    feature: version from cmakefile only

--- src/version.h
+++ src/version.h
@@ -3,14 +3,16 @@
 
 
 #include <stdio.h>
+#include <stdio.h>
 
+#ifndef __APP__VERSION__
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 8
 #define REVISION 5
 #define STRINGIFY(x) #x
 #define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
-
-
 static std::string version_string = VERSION_STR(VERSION_MAJOR, VERSION_MINOR, REVISION);
-
+#else
+static std::string version_string = __VERSION__; 
+#endif 
 #endif
