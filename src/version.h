4b043bc54d17f798f1050ea1fc8a039d3d1da6a9
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-02-17 19:49:15 +0200

    fix: version file

--- src/version.h
+++ src/version.h
@@ -5,14 +5,6 @@
 #include <stdio.h>
 #include <stdio.h>
 
-#ifndef __APP__VERSION__
-#define VERSION_MAJOR 2
-#define VERSION_MINOR 8
-#define REVISION 5
-#define STRINGIFY(x) #x
-#define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
-static std::string version_string = VERSION_STR(VERSION_MAJOR, VERSION_MINOR, REVISION);
-#else
+
 static std::string version_string = __APP__VERSION__; 
-#endif 
 #endif
