b196826d7400c7638dfa658b70a5518ac172b07a
Mohammad Hefny <mohammad.hefny@gmail.com>
2024-06-24 12:38:45 +0300

    update: color codes

--- src/helpers/colors.hpp
+++ src/helpers/colors.hpp
@@ -12,6 +12,9 @@
 #define _LOG_CONSOLE_BOLD_TEXT       "\033[1;34m"  // Blue
 #define _TEXT_BOLD_HIGHTLITED_       "\033[1;37m"  // WHITE
 #define _BOLD_CONSOLE_TEXT_          "\033[1m" // Restore normal console colour
+#define _BK_BLUE_WHITE_TEXT_         "\033[1;44;97m"
+#define _BK_WHITE_BLUE_TEXT_         "\033[1;34;47m"
+#define _BK_RED_WHITE_TEXT_          "\033[1;31;47m"
 #define _NORMAL_CONSOLE_TEXT_        "\033[0m" // Restore normal console colour
 
 
