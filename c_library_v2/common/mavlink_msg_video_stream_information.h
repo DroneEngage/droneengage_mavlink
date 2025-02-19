8e0c83787162c3d40c153839600728022c398fc5
HefnySco <mohammad.hefny@gmail.com>
2025-02-19 11:27:38 +0300

    fix

--- c_library_v2/common/mavlink_msg_video_stream_information.h
+++ c_library_v2/common/mavlink_msg_video_stream_information.h
@@ -306,20 +306,6 @@ static inline uint16_t mavlink_msg_video_stream_information_encode_status(uint8_
 }
 
 /**
- * @brief Encode a video_stream_information struct with provided status structure
- *
- * @param system_id ID of this system
- * @param component_id ID of this component (e.g. 200 for IMU)
- * @param status MAVLink status structure
- * @param msg The MAVLink message to compress the data into
- * @param video_stream_information C-struct to read the message contents from
- */
-static inline uint16_t mavlink_msg_video_stream_information_encode_status(uint8_t system_id, uint8_t component_id, mavlink_status_t* _status, mavlink_message_t* msg, const mavlink_video_stream_information_t* video_stream_information)
-{
-    return mavlink_msg_video_stream_information_pack_status(system_id, component_id, _status, msg,  video_stream_information->stream_id, video_stream_information->count, video_stream_information->type, video_stream_information->flags, video_stream_information->framerate, video_stream_information->resolution_h, video_stream_information->resolution_v, video_stream_information->bitrate, video_stream_information->rotation, video_stream_information->hfov, video_stream_information->name, video_stream_information->uri);
-}
-
-/**
  * @brief Send a video_stream_information message
  * @param chan MAVLink channel to send the message
  *
