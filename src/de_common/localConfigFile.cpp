e516bb3eb8710d65bd01ce583e5823f7743ae50a
HefnySco <mohammad.hefny@gmail.com>
2025-07-13 08:49:18 +0300

    feature: COMMON-CODE clocalConfigFile

--- src/de_common/localConfigFile.cpp
+++ src/de_common/localConfigFile.cpp
@@ -107,6 +107,24 @@ void CLocalConfigFile::addStringField(const char * field, const char * value)
     m_ConfigJSON[std::string(field)] = std::string(value);
 }
 
+
+void CLocalConfigFile::ModifyStringField(const char* field, const char* newValue)
+{
+    std::string key = std::string(field);
+    std::string value = std::string(newValue);
+
+    if (m_ConfigJSON.contains(key))
+    {
+        m_ConfigJSON[key] = value;
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Modified string field: " << _SUCCESS_CONSOLE_TEXT_ << field << _NORMAL_CONSOLE_TEXT_ << std::endl;
+    }
+    else
+    {
+        m_ConfigJSON[key] = value; // Add it if it doesn't exist
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Added new string field: " << _INFO_CONSOLE_TEXT << field << _NORMAL_CONSOLE_TEXT_ << " (field did not exist)" << std::endl;
+    }
+}
+
 std::string CLocalConfigFile::getStringField(const char * field) const
 {
     if (!m_ConfigJSON.contains(std::string(field))) return {};
@@ -126,4 +144,73 @@ const u_int32_t CLocalConfigFile::getNumericField(const char * field) const
     if (!m_ConfigJSON.contains(std::string(field))) return 0xffffffff;
 
     return m_ConfigJSON[std::string(field)].get<int>();
+}
+
+void CLocalConfigFile::ModifyNumericField(const char* field, const u_int32_t& newValue)
+{
+    std::string key = std::string(field);
+
+    if (m_ConfigJSON.contains(key))
+    {
+        m_ConfigJSON[key] = newValue;
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Modified numeric field: " << _SUCCESS_CONSOLE_TEXT_ << field << _NORMAL_CONSOLE_TEXT_ << std::endl;
+    }
+    else
+    {
+        m_ConfigJSON[key] = newValue; // Add it if it doesn't exist
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Added new numeric field: " << _INFO_CONSOLE_TEXT << field << _NORMAL_CONSOLE_TEXT_ << " (field did not exist)" << std::endl;
+    }
+}
+
+
+void CLocalConfigFile::removeFieldByName(const char* fieldName)
+{
+    std::string key = std::string(fieldName);
+    if (m_ConfigJSON.contains(key))
+    {
+        m_ConfigJSON.erase(key);
+        std::cout << _INFO_CONSOLE_BOLD_TEXT << "Removed field: " << _SUCCESS_CONSOLE_TEXT_ << fieldName << _NORMAL_CONSOLE_TEXT_ << std::endl;
+    }
+    else
+    {
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Field not found: " << _INFO_CONSOLE_TEXT << fieldName << _NORMAL_CONSOLE_TEXT_ << std::endl;
+    }
+}
+
+
+
+void CLocalConfigFile::addDoubleField(const char * field, double value)
+{
+    m_ConfigJSON[std::string(field)] = value;
+}
+
+double CLocalConfigFile::getDoubleField(const char * field) const
+{
+    std::string key = std::string(field);
+    if (!m_ConfigJSON.contains(key))
+    {
+        std::cout << _INFO_CONSOLE_TEXT << "Double field '" << key << "' not found." << _NORMAL_CONSOLE_TEXT_ << std::endl;
+        return 0.0; // Or some other default/error value
+    }
+    if (!m_ConfigJSON[key].is_number()) { // Check if it's any number (integer or float)
+        std::cout << _ERROR_CONSOLE_TEXT_ << "Field '" << key << "' is not a number." << _NORMAL_CONSOLE_TEXT_ << std::endl;
+        return 0.0;
+    }
+    return m_ConfigJSON[key].get<double>();
+}
+
+void CLocalConfigFile::ModifyDoubleField(const char* field, double newValue)
+{
+    std::string key = std::string(field);
+
+    if (m_ConfigJSON.contains(key))
+    {
+        m_ConfigJSON[key] = newValue;
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Modified double field: " << _SUCCESS_CONSOLE_TEXT_ << field << _NORMAL_CONSOLE_TEXT_ << std::endl;
+    }
+    else
+    {
+        m_ConfigJSON[key] = newValue;
+        std::cout << _LOG_CONSOLE_BOLD_TEXT << "Added new double field: " << _INFO_CONSOLE_TEXT << field << _NORMAL_CONSOLE_TEXT_ << " (field did not exist)" << std::endl;
+    }
 }
