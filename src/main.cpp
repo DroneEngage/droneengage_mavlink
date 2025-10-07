45f0edf30d9da0a851b8a0fb0b3caf321e7d23ec
HefnySco <mohammad.hefny@gmail.com>
2025-10-08 02:28:49 +0300

    fix logic: remove unneeded message

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,6 @@ using namespace de;
                         TYPE_AndruavMessage_P2P_STATUS, \
                         TYPE_AndruavMessage_Upload_DE_Mission, \
                         TYPE_AndruavMessage_CONFIG_ACTION, \
-                        TYPE_AndruavMessage_CONFIG_REMOTE_EXECUTE, \
                         TYPE_AndruavMessage_CONFIG_STATUS, \
                         TYPE_AndruavMessage_DUMMY}
 
