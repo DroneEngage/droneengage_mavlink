a368b94cd3bc282f6b5774f210329359c0a32b4a
HefnySco <mohammad.hefny@gmail.com>
2025-11-04 13:58:40 +0300

    fix logic & bug: tracking logic

--- src/fcb_andruav_message_parser.hpp
+++ src/fcb_andruav_message_parser.hpp
@@ -4,56 +4,59 @@
 #include <mavlink_command.h>
 #include <mavlink_sdk.h>
 
-
 #include "./de_common/helpers/json_nlohmann.hpp"
 using Json_de = nlohmann::json;
 #include "./de_common/de_databus/de_message_parser_base.hpp"
+#include "./mission/mission_manager.hpp"
 #include "./mission/missions.hpp"
 #include "./swarm/fcb_swarm_manager.hpp"
+#include "./tracking/fcb_tracking_manager.hpp"
 #include "fcb_facade.hpp"
-#include "fcb_traffic_optimizer.hpp"
 #include "fcb_main.hpp"
-#include "./mission/mission_manager.hpp"
+#include "fcb_traffic_optimizer.hpp"
 
-namespace de
-{
-namespace fcb
-{
+namespace de {
+namespace fcb {
 
-    /**
-     * @brief This class parses messages received via communicator and executes it.
-     * 
-     */
-    class CFCBAndruavMessageParser: public de::comm::CAndruavMessageParserBase
-    {
-    public:
-        static CFCBAndruavMessageParser& getInstance()
-        {
-            static CFCBAndruavMessageParser instance;
-            return instance;
-        }
+/**
+ * @brief This class parses messages received via communicator and executes it.
+ *
+ */
+class CFCBAndruavMessageParser : public de::comm::CAndruavMessageParserBase {
+public:
+  static CFCBAndruavMessageParser &getInstance() {
+    static CFCBAndruavMessageParser instance;
+    return instance;
+  }
 
-        CFCBAndruavMessageParser(CFCBAndruavMessageParser const&) = delete;
-        void operator=(CFCBAndruavMessageParser const&) = delete;
+  CFCBAndruavMessageParser(CFCBAndruavMessageParser const &) = delete;
+  void operator=(CFCBAndruavMessageParser const &) = delete;
 
-    private:
-        CFCBAndruavMessageParser() {}
+private:
+  CFCBAndruavMessageParser() {}
 
-    public:
-        ~CFCBAndruavMessageParser() {}
+public:
+  ~CFCBAndruavMessageParser() {}
 
-    protected:
-        void parseRemoteExecute(Json_de &andruav_message) override;
-        void parseCommand(Json_de &andruav_message, const char *full_message, const int &full_message_length, int messageType, uint32_t permission) override;
+protected:
+  void parseRemoteExecute(Json_de &andruav_message) override;
+  void parseCommand(Json_de &andruav_message, const char *full_message,
+                    const int &full_message_length, int messageType,
+                    uint32_t permission) override;
 
-    private:
-        de::fcb::CFCBMain& m_fcbMain = de::fcb::CFCBMain::getInstance();
-        mission::CMissionManager& m_mission_manager = mission::CMissionManager::getInstance();
-        mavlinksdk::CMavlinkSDK& m_mavlinksdk = mavlinksdk::CMavlinkSDK::getInstance();
-        de::fcb::CFCBFacade& m_fcb_facade = de::fcb::CFCBFacade::getInstance();
-        de::fcb::swarm::CSwarmManager& m_fcb_swarm_manager = de::fcb::swarm::CSwarmManager::getInstance();
-    };
-}
-}
+private:
+  de::fcb::CFCBMain &m_fcbMain = de::fcb::CFCBMain::getInstance();
+  mission::CMissionManager &m_mission_manager =
+      mission::CMissionManager::getInstance();
+  mavlinksdk::CMavlinkSDK &m_mavlinksdk =
+      mavlinksdk::CMavlinkSDK::getInstance();
+  de::fcb::CFCBFacade &m_fcb_facade = de::fcb::CFCBFacade::getInstance();
+  de::fcb::swarm::CSwarmManager &m_fcb_swarm_manager =
+      de::fcb::swarm::CSwarmManager::getInstance();
+  de::fcb::tracking::CTrackingManager &m_tracking_manager =
+      de::fcb::tracking::CTrackingManager::getInstance();
+};
+} // namespace fcb
+} // namespace de
 
 #endif
--- src/tracking/fcb_tracking_manager.hpp
+++ src/tracking/fcb_tracking_manager.hpp
@@ -7,86 +7,77 @@
 
 #include "pic_controller.hpp"
 
-
 #include "../de_common/helpers/json_nlohmann.hpp"
 using Json_de = nlohmann::json;
 
+namespace de {
+namespace fcb {
+namespace tracking {
+class CTrackingManager {
+public:
+  static CTrackingManager &getInstance() {
+    static CTrackingManager instance;
 
-namespace de
-{
-namespace fcb
-{
-namespace tracking
-{
-    class CTrackingManager
-    {
-        public:
-            static CTrackingManager& getInstance()
-                {
-                    static CTrackingManager instance;
+    return instance;
+  }
 
-                    return instance;
-                }
+  CTrackingManager(CTrackingManager const &) = delete;
+  void operator=(CTrackingManager const &) = delete;
 
-                CTrackingManager(CTrackingManager const&)            = delete;
-                void operator=(CTrackingManager const&)            = delete;
+private:
+  CTrackingManager() {}
 
-            
-                
-            private:
+public:
+  ~CTrackingManager() {}
 
-                CTrackingManager()
-                {
-                    
-                }
+public:
+  void init();
+  void onTrack(const double x, const double yz, const bool is_xy);
+  void onStatusChanged(const int status);
 
-                
-            public:
-                
-                ~CTrackingManager ()
-                {
+public:
+  inline void setParameters(const double x_PID_P, const double yz_PID_P,
+                            const double x_PID_I, const double yz_PID_I,
+                            const double alpha) {
+    m_x_PID_P = x_PID_P;
+    m_yz_PID_P = yz_PID_P;
+    m_x_PID_I = x_PID_I;
+    m_yz_PID_I = yz_PID_I;
+    m_alpha = alpha;
+  }
 
-                }
+private:
+  void getParameters();
 
+private:
+  bool m_tracking_running = false;
+  bool m_object_detected = false;
 
-            public:
-                
-                void init();
-                void onTrack(const double x, const double yz, const bool is_xy);
-                void onStatusChanged(const int status);
-            
-            public:
-                inline void setParameters(const double x_PID_P, const double yz_PID_P, const double x_PID_I, const double yz_PID_I, const double alpha)
-                {
-                    m_x_PID_P = x_PID_P;
-                    m_yz_PID_P = yz_PID_P;
-                    m_x_PID_I = x_PID_I;
-                    m_yz_PID_I = yz_PID_I;
-                    m_alpha = alpha;
-                }
-                
-            private:
+  CPIDController m_PID_X;
+  CPIDController m_PID_YZ;
 
-                bool m_tracking_running = false;
-                bool m_object_detected = false;
+  double m_x;
+  double m_yz;
+  double m_alpha = 0.1;
+  double m_x_PID_P = 1.0;
+  double m_yz_PID_P = 1.0;
+  double m_x_PID_I = 1.0;
+  double m_yz_PID_I = 1.0;
 
-                
-                CPIDController m_PID_X;
-                CPIDController m_PID_YZ;
-                
-                double m_x;
-                double m_yz;
-                double m_alpha = 0.1;
-                double m_x_PID_P = 1.0;
-                double m_yz_PID_P = 1.0;
-                double m_x_PID_I = 1.0;
-                double m_yz_PID_I = 1.0;
-                      
-                std::chrono::high_resolution_clock::time_point m_last_message_time;
-                const std::chrono::milliseconds m_target_frame_time_ms = std::chrono::milliseconds(500); 
-    };
-}
-}
-}
+  double m_expo_factor = 0.3;
+  double m_deadband = 0.01;
+  double m_rate_limit = 0.05; // max change per update in normalized units
+
+  double m_prev_dx = 0.0;
+  double m_prev_dy = 0.0;
+  bool m_prev_initialized = false;
+
+  std::chrono::high_resolution_clock::time_point m_last_message_time;
+  const std::chrono::milliseconds m_target_frame_time_ms =
+      std::chrono::milliseconds(500);
+};
+} // namespace tracking
+} // namespace fcb
+} // namespace de
 
 #endif
